792c648630b5adb9c79adc6993240a46327b34aa
goudacse <66686884+goudacse@users.noreply.github.com>
2025-08-29 00:01:57 +0530

    Update run-parallel.bat

--- run-parallel.bat
+++ run-parallel.bat
@@ -3,10 +3,10 @@ REM File: run-parallel.bat
 REM Purpose: Run multiple JMeter tests in parallel using Maven
 
 REM Start all tests in parallel
-start "End-to-End Test" cmd /c mvn clean verify -P end-to-end
-start "Add Products Test" cmd /c mvn clean verify -P add-products
-start "Add Pet Test" cmd /c mvn clean verify -P add-pet
-start "Add User Test" cmd /c mvn clean verify -P add-user
+start "End-to-End Test" cmd /c mvn clean verify -P end-to-end -DskipConfiguration=true
+start "Add Products Test" cmd /c mvn clean verify -P add-products -DskipConfiguration=true
+start "Add Pet Test" cmd /c mvn clean verify -P add-pet -DskipConfiguration=true
+start "Add User Test" cmd /c mvn clean verify -P add-user -DskipConfiguration=true
 
 REM Wait for all processes to complete
 echo Running tests in parallel...
